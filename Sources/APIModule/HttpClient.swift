b4e614d7938c54e73e7c22c665d3bc6279ff7937
Jijo Pulikkottil <jijo.pulikkottil@zcocorporation.com>
2024-02-22 10:51:44 +0530

    Added HttpClient with session data task, Specified supported platform and version in Package file

--- Sources/APIModule/HttpClient.swift
+++ Sources/APIModule/HttpClient.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+public class HttpClient {
+    
+    let session: URLSession
+    
+    public init(_ session: URLSession) {
+        self.session = session
+    }
+    
+    @discardableResult
+    public func performRequest(_ request: URLRequest) async throws -> (data: Data, response: URLResponse) {
+        
+        try await session.data(for: request)
+    }
+}
